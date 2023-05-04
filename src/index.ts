7bba9e395065778bccb6fec2e5648c304993a5d3
Tobshub <tobanigeorge@gmail.com>
2023-05-04 20:12:00 +0100

    appRouter

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ import express from "express";
 import cors from "cors";
 import { dbConn } from "./config/prisma";
 import logger from "./config/logger";
+import appRouter from "./app/router";
 
 const app = express();
 
 app.use(express.json(), cors());
+
+app.use(appRouter);
 
 dbConn().then((res) => {
   if (res.ok) {
