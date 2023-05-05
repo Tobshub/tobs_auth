2e1c7cc94f1a54873b5403665d8863f75cfeeb12
Tobshub <tobanigeorge@gmail.com>
2023-05-05 13:39:40 +0100

    add user router

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,5 +1,56 @@
+import logger from "@/config/logger";
+import prisma from "@/config/prisma";
+import { Err, Ok } from "@/config/result";
 import { Router } from "express";
+import z from "zod";
 
 const appRouter = Router();
+
+// add user
+appRouter.post("/user", async (req, res) => {
+  try {
+    const { user: data } = await req.body;
+    const zodTest = z.object({
+      email: z.string().email(),
+      password: z.string().min(8).max(64),
+    });
+
+    const user = zodTest.parse(data);
+
+    const checkUser = await prisma.user.findUnique({
+      where: { email: user.email },
+      select: { id: true },
+    });
+
+    if (checkUser) {
+      logger.warn("User already exists");
+      res.status(400).send(Err("User already exists"));
+      return;
+    }
+
+    const createUser = await prisma.user.create({
+      data: { email: user.email, password: user.password },
+      select: { id: true },
+    });
+
+    if (!createUser || !createUser.id) {
+      logger.error("Failed to create user");
+      res.status(500).send(Err("Failed to create user"));
+      return;
+    }
+
+    logger.info(user, "Login success");
+    res.status(200).send(Ok(createUser));
+  } catch (e) {
+    logger.error(e);
+    res.send(Err("An error occured"));
+  }
+});
+//
+// get user
+//
+// delete user
+//
+// update user
 
 export default appRouter;
