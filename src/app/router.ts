78a2bba5a8b386818676d2663fb9a243943add57
Tobshub <tobanigeorge@gmail.com>
2023-05-08 04:17:26 +0100

    misc

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import userRouter from "./users/userRouter";
 import { authWare } from "@/middlewares";
-import { Ok } from "@/config/result";
 import appToken from "./token";
 
 const appRouter = Router();
@@ -9,7 +8,7 @@ const appRouter = Router();
 appRouter.use("/user", authWare, userRouter);
 appRouter.use("/auth", (_, res) => {
   const newToken = appToken.generate();
-  res.status(200).send(Ok(newToken.value));
+  res.status(200).send(newToken);
 });
 
 export default appRouter;
