c4fd6f370e33395f3a893fc6329092cfc932e412
Tobshub <tobanigeorge@gmail.com>
2023-05-08 19:12:36 +0100

    misc

--- src/app/router.ts
+++ src/app/router.ts
@@ -7,9 +7,6 @@ import loginController from "./login";
 const appRouter = Router();
 
 appRouter.use("/user", authWare, userRouter);
-// FIXIT:
-// user token is the token for interacting with this service manually (through web page)
-// app token is the token for interacting with this service from another service
 // - login route to get token with credentials, web page to register services and get app tokens will use this
 appRouter.post("/login", loginController);
 // - /token route should have a userAuth middleware that makes sure that a valid user token is present to get an app token
