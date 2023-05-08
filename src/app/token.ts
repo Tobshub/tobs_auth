ed2630f196a96243cce79c2ad1ed37b74749cd76
Tobshub <tobanigeorge@gmail.com>
2023-05-08 14:32:02 +0100

    admin tokens

--- src/app/token.ts
+++ src/app/token.ts
@@ -3,17 +3,17 @@ import { Err, Ok } from "@/config/result";
 import jwt from "jsonwebtoken";
 import { ObjectId } from "bson";
 
-const jwtSecret = process.env.JWT_SECRET as string;
+const appSecret = process.env.JWT_SECRET as string;
 
-const appToken = {
+export const appToken = {
   generate: () => {
     const hex = new ObjectId();
-    const token = jwt.sign(hex.toString(), jwtSecret);
+    const token = jwt.sign(hex.toString(), appSecret);
     return Ok(token);
   },
   vaildate: (token: string) => {
     try {
-      const data = jwt.verify(token, jwtSecret);
+      const data = jwt.verify(token, appSecret);
       return Ok(data as string);
     } catch (e) {
       logger.warn("Token verification failed", e);
@@ -22,4 +22,21 @@ const appToken = {
   },
 };
 
-export default appToken;
+const adminSecret = process.env.ADMIN_SECRET as string;
+
+export const adminToken = {
+  generate: () => {
+    const hex = new ObjectId();
+    const token = jwt.sign(hex.toString(), adminSecret);
+    return Ok(token);
+  },
+  vaildate: (token: string) => {
+    try {
+      const data = jwt.verify(token, adminSecret);
+      return Ok(data as string);
+    } catch (e) {
+      logger.warn("Admin token verification failed", e);
+      return Err("Failed to verify token");
+    }
+  },
+}
