aaa9086c03241ef8477ae5279bf3ccda5519064e
Tobshub <tobanigeorge@gmail.com>
2023-05-08 04:12:59 +0100

    api tests

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import userRouter from "./users/userRouter";
 import { authWare } from "@/middlewares";
+import { Ok } from "@/config/result";
+import appToken from "./token";
 
 const appRouter = Router();
 
 appRouter.use("/user", authWare, userRouter);
 appRouter.use("/auth", (_, res) => {
-  
-})
+  const newToken = appToken.generate();
+  res.status(200).send(Ok(newToken.value));
+});
 
 export default appRouter;
--- test/app.test.js
+++ test/app.test.js
@@ -1,14 +1,66 @@
 const request = require("supertest");
 const { expect } = require("expect");
 
-const app = require("../dist/index.js").default;
+const app = "http://localhost:4000";
 
-describe("Testing the app", () => {
-  it("respond with html page", async () => {
-    const res = await request(app).get("/");
+describe("API endpoints testing", () => {
+  describe("GET /", () => {
+    it("respond with html page", async () => {
+      const res = await request(app).get("/");
 
-    expect(res.status).toBe(200);
-    expect(res.body).toBeTruthy();
-    expect(res.headers["content-type"]).toContain("text/html");
+      expect(res.status).toBe(200);
+      expect(res.body).toBeTruthy();
+      expect(res.headers["content-type"]).toContain("text/html");
+    });
+  });
+
+  describe("GET /api/user", () => {
+    it("should error because there's no token", async () => {
+      const res = await request(app).get("/api/user/");
+
+      expect(res.status).toBe(401);
+      expect(await res.body).toMatchObject({
+        ok: false,
+        message: "Invalid Authorization Token",
+        cause: "Token is missing",
+      });
+    });
+
+    it("should error because token is invalid", async () => {
+      const res = await request(app)
+        .get("/api/user")
+        .set({ Authorization: "fake_token" });
+
+      expect(res.status).toBe(401);
+      expect(await res.body).toMatchObject({
+        ok: false,
+        message: "Invalid Authorization Token",
+      });
+    });
+
+    it("should fetch a new token and then try to get a user", async () => {
+      const reqToken = await request(app).get("/api/auth");
+
+      expect(reqToken.status).toBe(200);
+
+      const reqTokenBody = await reqToken.body;
+      expect(reqTokenBody).toHaveProperty("ok", true);
+      expect(reqTokenBody.value).toBeTruthy();
+      expect(typeof reqTokenBody.value).toBe("string");
+
+      const token = await reqToken.body.value;
+      const email = "test@test.com";
+
+      const res = await request(app)
+        .get(`/api/user?email=${email}`)
+        .set({ Authorization: `Bearer ${token}` });
+      const resBody = await res.body;
+
+      expect(res.status).toBe(200);
+      expect(resBody).toHaveProperty("ok", true);
+      expect(resBody.value).toBeDefined();
+      expect(resBody.value.email).toBe(email);
+      expect(resBody.value.id).toBeTruthy();
+    });
   });
 });
