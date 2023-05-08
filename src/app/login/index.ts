233f3696a305560703b67f8776f51987418c5e7d
Tobshub <tobanigeorge@gmail.com>
2023-05-08 14:40:44 +0100

    admin login route

---
+++ src/app/login/index.ts
@@ -0,0 +1,61 @@
+import logger from "@/config/logger";
+import { Err, Ok } from "@/config/result";
+import { Request, Response } from "express";
+import z from "zod";
+import { adminToken } from "../token";
+
+type User = { email: string; password: string };
+
+export default async function loginController(req: Request, res: Response) {
+  const { rawUser } = await req.body;
+  const user = await validateLoginInput(rawUser).then((res) => {
+    if (res.ok) {
+      return res.value;
+    }
+    return null;
+  });
+
+  if (!user) {
+    res.status(400).send(Err("Login Failed", "User object is missing"));
+    return;
+  }
+
+  const valid = verfiyUser(user);
+
+  if (valid.ok) {
+    res.status(200).send(valid);
+    return;
+  }
+
+  res.status(401).send(valid);
+}
+
+function verfiyUser(user: User) {
+  const actualUser: User = {
+    email: process.env.ADMIN_EMAIL as string,
+    password: process.env.ADMIN_PASSWORD as string,
+  };
+
+  if (
+    actualUser.email.toLowerCase() === user.email.toLowerCase() &&
+    actualUser.password === user.password
+  ) {
+    const token = adminToken.generate();
+    return token;
+  }
+
+  logger.warn("Login attempt with wrong credentials");
+  return Err("Email or Password is wrong");
+}
+
+verfiyUser({ email: "", password: "" });
+
+async function validateLoginInput(user: User) {
+  try {
+    const test = z.object({ email: z.string().email(), password: z.string() });
+    const validatedInput = test.parse(user);
+    return Ok(validatedInput);
+  } catch (e) {
+    return Err("Invalid input");
+  }
+}
--- src/app/router.ts
+++ src/app/router.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import userRouter from "./users/userRouter";
 import { authWare } from "@/middlewares";
-import appToken from "./token";
+import { appToken } from "./token";
+import loginController from "./login";
 
 const appRouter = Router();
 
 appRouter.use("/user", authWare, userRouter);
 // FIXIT:
-// user token is the token for interacting with this service manually
+// user token is the token for interacting with this service manually (through web page)
 // app token is the token for interacting with this service from another service
 // - login route to get token with credentials, web page to register services and get app tokens will use this
+appRouter.post("/login", loginController);
 // - /token route should have a userAuth middleware that makes sure that a valid user token is present to get an app token
 appRouter.get("/token", (_, res) => {
   const newToken = appToken.generate();
