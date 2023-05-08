8979d05d56f147180579686ac61c8d0e52765b08
Tobshub <tobanigeorge@gmail.com>
2023-05-08 14:37:33 +0100

    warn in all envs

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,13 +1,11 @@
 import pino from "pino";
 
 const LOG = pino({ transport: { target: "pino-pretty" } });
-const env = process.env.NODE_ENV;
 
 const logger = {
   info: (obj: any, msg?: any, ...args: any) => LOG.info(obj, msg, ...args),
   error: (obj: any, msg?: any, ...args: any) => LOG.error(obj, msg, ...args),
-  warn: (obj: any, msg?: any, ...args: any) =>
-    !env || env === "development" ? LOG.warn(obj, msg, ...args) : null,
+  warn: (obj: any, msg?: any, ...args: any) => LOG.warn(obj, msg, ...args),
 };
 
 export default logger;
