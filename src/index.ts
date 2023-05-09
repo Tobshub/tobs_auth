9e2ac1758780b567ca6cbe243c63e4171f1ec3ff
Tobshub <tobanigeorge@gmail.com>
2023-05-09 20:43:37 +0100

    web page

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,20 @@ import cors from "cors";
 import { dbConn } from "./config/prisma";
 import logger from "./config/logger";
 import appRouter from "./app/router";
+import path from "path";
 
 const app = express();
 
-app.use(express.json(), cors());
+app.use(
+  express.json(),
+  cors(),
+  express.static(path.join(process.cwd(), "public"), { dotfiles: "deny" })
+);
+
 app.use("/api", appRouter);
 
 app.use("/", (_, res) => {
-  res.contentType("text/html");
-  res.send("<h1>Hello world</h1>");
+  res.sendFile(path.join(process.cwd(), "public/index.html"));
 });
 
 dbConn().then((res) => {
